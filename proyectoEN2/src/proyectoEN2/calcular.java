bdb7b4d3ffa95d65109336d111c3accc9ff4eee2
Brailyn León <Brailyn Len@DESKTOP-FV20U26>
2024-04-02 13:46:28 +0200

    prueba_modificada

--- proyectoEN2/src/proyectoEN2/calcular.java
+++ proyectoEN2/src/proyectoEN2/calcular.java
@@ -4,6 +4,7 @@ public class calcular {
     public static void main(String[] args) {
         for (int i = 0; i <= 10; i++) {
             System.out.println("holaaaaaaaaaaa");
+            System.out.println("hello");
         }
     }
 }
